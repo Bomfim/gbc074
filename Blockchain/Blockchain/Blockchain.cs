fc596de31db6951218b54eefffae28d0b0817d1d
Bomfim <mariocesarbomfim@gmail.com>
2019-09-15 10:33:29 -0300

    Blockchainformation...

---
+++ Blockchain/Blockchain/Blockchain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blockchain
+{
+    class Blockchain
+    {
+        public IList<Block> Chain { get; set; }
+
+        public Blockchain()
+        {
+            InitializeChain();
+            AddGenesisBlock();
+        }
+
+        public void InitializeChain()
+        {
+            Chain = new List<Block>();
+        }
+
+        private Block CreateGenesisBlock()
+        {
+            return new Block(DateTime.Now, null, "{}");
+        }
+
+        public void AddGenesisBlock()
+        {
+            Chain.Add(CreateGenesisBlock());
+        }
+
+        public Block GetLatestBlock()
+        {
+            return Chain[Chain.Count - 1];
+        }
+
+        public void AddBlock(Block block)
+        {
+            Block latestBlock = GetLatestBlock();
+            block.Index = latestBlock.Index + 1;
+            block.PreviousHash = latestBlock.Hash;
+            block.Hash = block.CalculateHash();
+            Chain.Add(block);
+        }
+
+    }
+}
