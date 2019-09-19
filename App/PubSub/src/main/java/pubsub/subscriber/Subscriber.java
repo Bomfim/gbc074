7640aa2e890f79ab2f7f9f215495f7ccdde6d96a
Bomfim <mariocesarbomfim@gmail.com>
2019-09-19 16:33:13 -0300

    PubSub working =)

---
+++ App/PubSub/src/main/java/pubsub/subscriber/Subscriber.java
@@ -0,0 +1,36 @@
+package pubsub.subscriber;
+
+import java.util.ArrayList;
+import java.util.List;
+import pubsub.Message;
+import pubsub.service.PubSubService;
+
+public abstract class Subscriber {
+
+	// Store all messages received by the subscriber.
+	private List<Message> subscriberMessages = new ArrayList<Message>();
+
+	public List<Message> getSubscriberMessages() {
+		return subscriberMessages;
+	}
+
+	public void setSubscriberMessages(List<Message> subscriberMessages) {
+		this.subscriberMessages = subscriberMessages;
+	}
+
+	/// Add subscriber with PubSubService for a match.
+	public abstract void addSubscriber(String match, PubSubService pubSubService);
+
+	// Unsubscribe subscriber with PubSubService for a match.
+	public abstract void unSubscribe(String match, PubSubService pubSubService);
+
+	// Request specifically for messages related to topic from PubSubService
+	public abstract void getMessagesForSubscriberOfMatch(String match, PubSubService pubSubService);
+
+	// Print all messages received by the subscriber
+	public void printMessages() {
+		for (Message message : subscriberMessages) {
+			System.out.println("Match Message –––> " + message.getMatch() + " : " + message.getPayload());
+		}
+	}
+}
---
+++ App/PubSub/src/main/java/pubsub/subscriber/SubscriberImpl.java
@@ -0,0 +1,28 @@
+package pubsub.subscriber;
+
+import pubsub.service.PubSubService;
+
+public class SubscriberImpl extends Subscriber {
+
+	// Add subscriber with PubSubService for a match.
+	@Override
+	public void addSubscriber(String match, PubSubService pubSubService) {
+		pubSubService.addSubscriber(match, this);
+
+	}
+
+	// Unsubscribe subscriber with PubSubService for a match
+	@Override
+	public void unSubscribe(String match, PubSubService pubSubService) {
+		pubSubService.removeSubscriber(match, this);
+
+	}
+
+	// Request specifically for messages related to a match from PubSubService
+	@Override
+	public void getMessagesForSubscriberOfMatch(String match, PubSubService pubSubService) {
+		pubSubService.getMessagesForSubscriberOfTopic(match, this);
+
+	}
+
+}
