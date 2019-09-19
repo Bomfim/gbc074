2d6e83e1bb8999a4390c0f37b87e0d3a28a13fca
Mario Melon Bomfim <mmelonbo@everis.com>
2019-09-19 15:37:05 -0300

    PubSub created!

---
+++ App/PubSub/src/main/java/pubsub/publisher/PublisherImpl.java
@@ -0,0 +1,13 @@
+package pubsub.publisher;
+
+import pubsub.Message;
+import pubsub.service.PubSubService;
+
+public class PublisherImpl implements Publisher {
+
+	public void publish(Message message, PubSubService pubSubService) {
+		pubSubService.addMessageToQueue(message);
+
+	}
+
+}
