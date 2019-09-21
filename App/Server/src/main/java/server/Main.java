584617db981db1c9af901fdbd1e481611e85425a
bruno.fernandes <bruno.fernandes@dual.hu>
2019-09-20 22:47:05 -0300

    CRUD and testCRUD

--- App/Server/src/main/java/server/Main.java
+++ App/Server/src/main/java/server/Main.java
@@ -1,14 +1,20 @@
 package server;
 
+import dao.MatchDao;
+import entity.Match;
+import pubsub.service.PubSubService;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 
-import pubsub.service.PubSubService;
 
 public class Main {
 
 	public static void main(String args[]) {
+
+		//testCRUD();
 
 		int port = 50051;
 		boolean isReporter = false;
@@ -35,5 +41,31 @@ public class Main {
 			System.out.println("Server exception: " + ex.getMessage());
 			ex.printStackTrace();
 		}
+	}
+
+	public void testCRUD(){
+		MatchDao dao = new MatchDao();
+		Match match = new Match("COR", "SAO");
+		match.setInProgress(true);
+		match.setHostTeamScore(2);
+		match.setGuestTeamScore(1);
+		match.setReporter("Bruno");
+		match.setComments(Arrays.asList("gol","gol de novo"));
+
+		//Create
+		dao.persist(match);
+
+		//Read
+		Match createdMatch = dao.findById(match.getId());
+		System.out.println(createdMatch);
+
+		//Update
+		createdMatch.setHostTeamScore(3);
+		dao.edit(createdMatch);
+		System.out.println(dao.findById(createdMatch.getId()));
+
+		//Delete
+		dao.delete(createdMatch);
+		System.out.println(dao.readAll());
 	}
 }
